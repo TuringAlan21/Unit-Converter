4b8c4a2f3017a0b01ba7b1faef96d38910012aa5
Lasha <lashachachua1010@gmail.com>
2024-11-06 16:59:44 +0400

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ inputEl.value = "";
 
 
 convertBtn.addEventListener("click", function(){
-    const metersToFeet = toFixed(2)(inputEl.value * 3.28084);
-    const feetToMeters = toFixed(2)(inputEl.value / 3.28084);
-    const litersToGallons = toFixed(2)(inputEl.value / 3.785);
-    const gallonsToLiters = toFixed(2)(inputEl.value * 3.785);
-    const kilogramsToPounds = toFixed(2)(inputEl.value * 2.205);
-    const poundsToKilograms = toFixed(2)(inputEl.value / 2.205);
+    const metersToFeet = toFixed(2).inputEl.value * 3.28084;
+    const feetToMeters = toFixed(2).inputEl.value / 3.28084;
+    const litersToGallons = toFixed(2).inputEl.value / 3.785;
+    const gallonsToLiters = toFixed(2).inputEl.value * 3.785;
+    const kilogramsToPounds = toFixed(2).inputEl.value * 2.205;
+    const poundsToKilograms = toFixed(2).inputEl.value / 2.205;
 
 
     convertedLength.textContent = `${inputEl.value} meters = ${metersToFeet} feet | ${inputEl.value} feet = ${feetToMeters} meters`
